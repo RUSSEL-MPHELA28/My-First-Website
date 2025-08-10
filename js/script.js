2bbf80a7fbb4b9c0a8f9d2c813faf4650fcf5685
Russel Mphela <26981254@mylife.unisa.ac.za>
2025-08-10 16:29:55 +0200

    content section js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
+const  navLinks = document.querySelectorAll('.nav-link');
 
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
@@ -10,4 +11,20 @@ document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", 
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }));
+
+navLinks.forEach(link => {
+    link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetID = link.getAttribute('href');
+
+        const contentSection = document.querySelectorAll('.content-section');
+        contentSection.forEach(section  => {
+            section.classList.add('hidden');
+        });
+
+        setTimeout(() => {
+            document.querySelector(targetID).classList.remove('hidden');
+        }, 500)
+    })
+});
 
